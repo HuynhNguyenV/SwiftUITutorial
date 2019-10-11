306eb271ab08c1398c1fc71b6e9623b1aee58deb
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2019-10-11 20:44:58 +0700

    zstack-introduction

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -10,43 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            Text("HStack").font(.largeTitle)
-            Text("Layout Priority")
-                .font(.title)
-                .foregroundColor(.gray)
-            Text("Use the layout priority modifier to give priority to the space a view needs to show its content.")
-                .frame(maxWidth: .infinity)
-                .padding().layoutPriority(1)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .font(.title)
+        ZStack {
+            Color.gray
             
-            Text(".layoutPriority(1) is set on 'Brings Balance'").font(.subheadline)
-            
-            HStack {
-                Text("SwiftUI").font(.largeTitle).lineLimit(1)
-                Image(systemName: "pencil.circle.fill")
-                    .resizable() // fill size
-                    .frame(width: 80, height: 80)
-                Text("Brings Balance")
-                    .font(.largeTitle)
-                    .layoutPriority(1)
-            }.padding(.horizontal)
-            
-            HStack {
-                Text("SwiftUI")
-                    .font(.largeTitle)
-                    .layoutPriority(1)
-                Image(systemName: "pencil.circle.fill")
-                    .resizable() // fill size
-                    .frame(width: 80, height: 80)
-                Text("Brings Balance")
-                    .font(.largeTitle)
-                    .lineLimit(1)
-            }.padding(.horizontal)
-            
-            Text(".layoutPriority(1) is set on 'SwiftUI'").font(.subheadline)
+            VStack(spacing: 20) {
+                Text("ZStack").font(.largeTitle)
+                Text("Introduction").foregroundColor(.white)
+                
+                Text("ZStacks are great for setting a background color.")
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.yellow)
+                    
+                Text("But notice the Color stops at the Safe Areas (white areas on top and botom).")
+                    .frame(maxWidth: .infinity, maxHeight: 120)
+                    .padding()
+                    .background(Color.yellow)
+            }
+            .font(.title)
         }
     }
 }
