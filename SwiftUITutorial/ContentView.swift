7343c4a097f7c50b9aff8f06f8567c54e242d80f
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2019-10-11 16:17:40 +0700

    introduction

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -10,7 +10,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 20) {
+            Text("HStack").font(.largeTitle)
+            Text("Introduction")
+                .font(.title)
+                .foregroundColor(.gray)
+            Text("HStacks are views tha contain other views laid out horizontally.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .font(.title)
+                .layoutPriority(1)
+            HStack {
+                Text("Leading")
+                Text("Moddle")
+                Text("Trailing")
+            }
+            .padding()
+            .border(Color.blue)
+            
+            HStack(spacing: 10) {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+            }
+            
+            HStack(spacing: 20) {
+                Image(systemName: "a.circle.fill")
+                Image(systemName: "b.circle.fill")
+                Image(systemName: "c.circle.fill")
+                Image(systemName: "d.circle.fill")
+                Image(systemName: "e.circle.fill")
+            }
+            .font(.largeTitle).padding()
+            .background(RoundedRectangle(cornerRadius: 10)
+            .foregroundColor(.blue))
+            .foregroundColor(.white)
+            
+        }
     }
 }
 
