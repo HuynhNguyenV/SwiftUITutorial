606ab89799ba47b542463e51b715c43422eb3b12
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2019-10-15 20:50:56 +0700

    From a Specific Date or Time

---
+++ SwiftUITutorial/ColorCommon.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  SwiftUITutorial
+//
+//  Created by Macintosh HD on 10/15/19.
+//  Copyright © 2019 Huynh. All rights reserved.
+//
+
+import SwiftUI
+
+class ColorCommon: UIColor {
+    static let pinkCustom = UIColor(red:0.95, green:0.72, blue:0.85, alpha:1.0)
+}
+
--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,25 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var dateInForm = Date()
+    @State private var arrivalDate = Date()
+    
+    let fromToday = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
+    
+    
     
     var body: some View {
-        Form {
-            Section(header: Text("DatePicker").font(.largeTitle).padding()) {
-                Text("Used In Forms")
-                    .font(.title)
-                    .foregroundColor(.gray)
-                    .padding()
-                
-                Text("The date picker looks differently when used in a form. The first parameter called 'title' is used when in forms and lists.")
-                    .frame(maxWidth: .infinity)
-                    .padding().listRowBackground(Color.yellow)
-                    .font(.title)
-                
-                DatePicker("DatePicker Collapsed (Default)", selection: $dateInForm, displayedComponents: .date)
-                DatePicker("DatePicker Collapsed (Selected)", selection: $dateInForm, displayedComponents: .date)
-                
-            }
+        VStack(spacing: 10) {
+            Text("DatePicker").font(.largeTitle)
+            Text("From Date or Time")
+                .font(.title).foregroundColor(.gray)
+            
+            Image("catalina")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(.horizontal)
+            
+            Text("Arrival Date").font(.title)
+            
+            DatePicker("", selection: $arrivalDate, in: fromToday..., displayedComponents: .date)
+                .offset(x: -50, y: 0)
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color.init(ColorCommon.pinkCustom))
+                    
+                .padding(.horizontal))
+            
+            Spacer()
         }
     }
 }
