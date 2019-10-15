3259d134176188145f5de0cef4ee49706f605e49
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2019-10-15 14:10:02 +0700

    custom button

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -10,34 +10,99 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            Text("Button").font(.largeTitle)
-            
-            Button("Default Button Style") {
+        ZStack {
+            VStack(spacing: 10) {
+                Text("Button").font(.largeTitle)
+                
+                Button(action: { print("New User") }) {
+                    VStack {
+                        Text("New User").font(.title)
+                        Text("(Register Here)")
+                    }
+                }
+                
+                Button(action: { print("Forgot Password") }) {
+                    HStack {
+                        Text("Forgot Password?").foregroundColor(.black)
+                        Text("Tap to Recover").foregroundColor(.blue)
+                    }.font(.title)
+                }
+                
+                HStack {
+                    Button(action: {}) {
+                        Text("Solid Button")
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    Button(action: {}) {
+                        Text("Border Button")
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.blue, lineWidth: 2))
+                    }
+                }
+                
+                Button(action: {}) {
+                    Text("Button With Rounded Ends")
+                        .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(.infinity)
+                }
+                
+                HStack {
+                    Button(action: {}) {
+                        HStack {
+                            Text("Symbol").padding(.horizontal)
+                            Image(systemName: "gift.fill")
+                        }.padding()
+                    }
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    
+                    Button(action: {}) {
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                            Text("Search").padding(.horizontal)
+                        }.padding()
+                    }
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(.infinity)
+                }
+                
+                Button(action: {}) {
+                    VStack {
+                        Image(systemName: "video.fill")
+                        Text("Record").padding(.horizontal)
+                    }.padding()
+                }
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(.infinity)
                 
             }
-            Divider()
-            Button(action: {
-                print("Headline Font")
-            }) {
-                Text("Headline Font").font(.headline)
-            }
-            Divider()
             
-            Button(action: {
-                print("Foreground Color")
-            }){
-                Text("Foreground Color").foregroundColor(.red)
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "plus").font(.largeTitle)
+                    }
+                    .padding(20)
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(.infinity)
+                }.padding(.trailing, 30)
             }
-            Divider()
-            Button(action: {
-                print("Thin Font Weight")
-            }) {
-                Text("Thin Font Weight").fontWeight(.thin)
-            }
-            
-            
-        }.font(.title)
+        }
+        
     }
 }
 
