45fc0b2965edd49a7102519d62b2e4f51c59638c
HuynhNguyenV <nguyenviethuynh1996.vih@gmail.com>
2019-10-15 16:02:07 +0700

    introduction

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,98 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var nextFullMoonDate = Date()
+    
     var body: some View {
-        ZStack {
-            VStack(spacing: 10) {
-                Text("Button").font(.largeTitle)
-                
-                Button(action: { print("New User") }) {
-                    VStack {
-                        Text("New User").font(.title)
-                        Text("(Register Here)")
-                    }
-                }
-                
-                Button(action: { print("Forgot Password") }) {
-                    HStack {
-                        Text("Forgot Password?").foregroundColor(.black)
-                        Text("Tap to Recover").foregroundColor(.blue)
-                    }.font(.title)
-                }
-                
-                HStack {
-                    Button(action: {}) {
-                        Text("Solid Button")
-                            .padding()
-                            .background(Color.blue)
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                    }
-                    
-                    Button(action: {}) {
-                        Text("Border Button")
-                            .padding()
-                            .background(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.blue, lineWidth: 2))
-                    }
-                }
-                
-                Button(action: {}) {
-                    Text("Button With Rounded Ends")
-                        .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(.infinity)
-                }
-                
-                HStack {
-                    Button(action: {}) {
-                        HStack {
-                            Text("Symbol").padding(.horizontal)
-                            Image(systemName: "gift.fill")
-                        }.padding()
-                    }
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(8)
-                    
-                    Button(action: {}) {
-                        HStack {
-                            Image(systemName: "magnifyingglass")
-                            Text("Search").padding(.horizontal)
-                        }.padding()
-                    }
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(.infinity)
-                }
-                
-                Button(action: {}) {
-                    VStack {
-                        Image(systemName: "video.fill")
-                        Text("Record").padding(.horizontal)
-                    }.padding()
-                }
-                .background(Color.red)
-                .foregroundColor(.white)
-                .cornerRadius(.infinity)
-                
-            }
+        VStack(spacing: 20){
+            Text("DatePicker").font(.largeTitle)
+            Text("Introduction")
+                .font(.title)
+                .foregroundColor(.gray)
             
-            VStack {
+            Text("Binf a date variable to get and set the date in the date picker")
+                .frame(maxWidth: .infinity).padding()
+                .background(Color.yellow)
+                .font(.title)
+            
+            Text("Next Full Moon").font(.largeTitle)
+            
+            HStack {
+                Spacer(minLength: 50)
+                Image(systemName: "moon.circle")
                 Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {}) {
-                        Image(systemName: "plus").font(.largeTitle)
-                    }
-                    .padding(20)
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(.infinity)
-                }.padding(.trailing, 30)
-            }
+                Circle().frame(height: 60.0)
+                Spacer()
+                Image(systemName: "moon.circle.fill")
+                Spacer(minLength: 50)
+            }.font(.largeTitle)
+                .foregroundColor(.yellow)
+            
+            Text("Date of next full moom")
+            
+            DatePicker("", selection: $nextFullMoonDate, displayedComponents: .date)
+            Spacer()
         }
         
     }
