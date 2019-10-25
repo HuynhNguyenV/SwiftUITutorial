3301acc7fbd5ccce9b57e73b6f89fe463d9bd8a4
Huynh Nguyen V <nguyenviethuynh1996.vih@gmail.com>
2019-10-25 20:47:29 +0700

    With minimum and maximum date range

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,32 +9,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var arrivalDate = Date()
+    @State private var nextFullDateMoon = Date()
     
     let fromToday = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
     
+    var withinNext30Days: ClosedRange<Date> {
+        let today = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
+        let next30Days = Calendar.current.date(byAdding: .day, value: 30, to: Date())!
+        return today...next30Days
+    }
     
     
     var body: some View {
-        VStack(spacing: 10) {
+        VStack(spacing: 20) {
             Text("DatePicker").font(.largeTitle)
-            Text("From Date or Time")
-                .font(.title).foregroundColor(.gray)
+            Text("Min and Max Date Range").foregroundColor(.gray).font(.title)
             
-            Image("catalina")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(.horizontal)
+            HStack {
+                Spacer(minLength: 60)
+                Image(systemName: "moon.circle").font(.title)
+                Spacer()
+                Circle().frame(height: 60.0)
+                Spacer()
+                Image(systemName: "moon.circle.fill").font(.title)
+                Spacer(minLength: 60)
+            }.foregroundColor(.yellow)
             
-            Text("Arrival Date").font(.title)
+            Text("Select date of next full moon").font(.title)
             
-            DatePicker("", selection: $arrivalDate, in: fromToday..., displayedComponents: .date)
+            DatePicker("", selection: $nextFullDateMoon, in: withinNext30Days, displayedComponents: .date)
                 .offset(x: -50, y: 0)
-                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color.init(ColorCommon.pinkCustom))
-                    
-                .padding(.horizontal))
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.yellow))
+                .padding(.horizontal)
+                .shadow(radius: 20, y: 20)
+            
+            Text("(Valid range only in the next 30 days)").padding(.vertical)
             
             Spacer()
+            
         }
     }
 }
