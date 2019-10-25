0d02d49e96ff471b12915ab9ba522e566b60fa01
Huynh Nguyen V <nguyenviethuynh1996.vih@gmail.com>
2019-10-25 21:38:01 +0700

    form introduction

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,44 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var nextFullDateMoon = Date()
-    
-    let fromToday = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
-    
-    var withinNext30Days: ClosedRange<Date> {
-        let today = Calendar.current.date(byAdding: .minute, value: -1, to: Date())!
-        let next30Days = Calendar.current.date(byAdding: .day, value: 30, to: Date())!
-        return today...next30Days
-    }
-    
-    
+
     var body: some View {
-        VStack(spacing: 20) {
-            Text("DatePicker").font(.largeTitle)
-            Text("Min and Max Date Range").foregroundColor(.gray).font(.title)
+        Form {
+            Section {
+                Text("This is a Form").font(.title)
+                Text("You Can put any content in here")
+                Text("The cells with grow to fit the content")
+                Text("Remember, it's just views inside of views")
+            }
             
-            HStack {
-                Spacer(minLength: 60)
-                Image(systemName: "moon.circle").font(.title)
-                Spacer()
-                Circle().frame(height: 60.0)
-                Spacer()
-                Image(systemName: "moon.circle.fill").font(.title)
-                Spacer(minLength: 60)
-            }.foregroundColor(.yellow)
+            Section {
+                Text("Limitations").font(.title)
+                Text("There are build-in margins thar are difficult to get around. Take s look at the color below so you can see where the margins are:")
+                Color.green
+            }
             
-            Text("Select date of next full moon").font(.title)
-            
-            DatePicker("", selection: $nextFullDateMoon, in: withinNext30Days, displayedComponents: .date)
-                .offset(x: -50, y: 0)
-                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.yellow))
-                .padding(.horizontal)
-                .shadow(radius: 20, y: 20)
-            
-            Text("(Valid range only in the next 30 days)").padding(.vertical)
-            
-            Spacer()
-            
+            Section {
+                Text("Summary").font(.title)
+                Text("Pretty much what you see here is what you get.")
+            }
         }
     }
 }
